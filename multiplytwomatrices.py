b87f7984eaef2cd3bb2f3268bbd213eb692fa7a5
Chai24092002 <chaithravinu2002@gmail.com>
2025-02-07 15:06:37 +0530

    Create  multiplytwomatrices.py

---
+++ multiplytwomatrices.py
@@ -0,0 +1,9 @@
+A = [[1, 2], [3, 4]]
+B = [[5, 6], [7, 8]]
+
+# Multiply matrices
+result = [[A[i][0] * B[0][j] + A[i][1] * B[1][j] for j in range(2)] for i in range(2)]
+
+# Print result
+for row in result:
+    print(row)
